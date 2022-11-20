f8ee6ed7750dfb2dd419ae09e055e18b49ef6f2c
Tony Hristov <tony.hristov@alkami.com>
2022-11-20 17:36:37 -0600

    chore(airship) Packages and code

--- airship_messaging/lib/main.dart
+++ airship_messaging/lib/main.dart
@@ -1,115 +1,171 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:airship_flutter/airship_flutter.dart' as airship;
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> _backgroundMessageHandler(Map<String, dynamic> payload, airship.Notification? notification) async {
+  log("[airship] _backgroundMessageHandler: payload=$payload, notification=$notification");
+}
+
+void _channelRegistrationHandler(airship.ChannelEvent event) {
+  log("[airship] _channelRegistrationHandler: channelid=${event.channelId}, registrationToken=${event.registrationToken}");
+}
+
+void _pushReceivedHandler(airship.PushReceivedEvent event) {
+  log("[airship] _pushReceivedHandler: event=$event");
+}
+
+void _notificationResponseHandler(airship.NotificationResponseEvent event) {
+  log("[airship] _notificationResponseHandler: event=$event");
+}
+
+void _deepLinkHandler(String? event) {
+  log("[airship] _deepLinkHandler: event=$event");
+}
+
+Future<String> getChannelId() async {
+  if (!await _canUseAirship) {
+    return '';
+  }
+
+  final channelId = await airship.Airship.channelId;
+  log('[airship] channelId=$channelId');
+  return Future.value(channelId ?? '');
+}
+
+Future<bool> get _canUseAirship async {
+  final canRunAirship = Platform.isIOS ? (await DeviceInfoPlugin().iosInfo).isPhysicalDevice : Platform.isAndroid;
+  final permissionGranted = await Permission.notification.request().isGranted;
+  return canRunAirship && permissionGranted;
+}
+
+void main() async {
+  // Required to make the main function async
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  String errMsg = '';
+
+  if (await _canUseAirship) {
+    try {
+      // Enable notifications (prompts on iOS)
+      airship.Airship.setUserNotificationsEnabled(true);
+
+      await airship.Airship.setBackgroundMessageHandler(_backgroundMessageHandler);
+
+      airship.Airship.onChannelRegistration.listen(_channelRegistrationHandler);
+
+      // Triggered when push notification arrives
+      airship.Airship.onPushReceived.listen(_pushReceivedHandler);
+
+      // Triggered when app is resumed or started from notification tap
+      airship.Airship.onNotificationResponse.listen(_notificationResponseHandler);
+
+      // Triggered when a push notificatioflutterflutter n containing a deep link is tapped
+      airship.Airship.onDeepLink.listen(_deepLinkHandler);
+    } catch (e) {
+      errMsg = e.toString();
+    }
+  }
+
+  runApp(MyApp(errMsg: errMsg));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key, required this.errMsg}) : super(key: key);
+
+  final String errMsg;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Airhip Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(errMsg: errMsg),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key, required this.errMsg}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final String errMsg;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Future<String>? _getChannelId() => errMsg.isNotEmpty ? Future.value('') : getChannelId();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      backgroundColor: Colors.white,
+      body: DefaultTextStyle(
+        style: Theme.of(context).textTheme.headline5!,
+        textAlign: TextAlign.center,
+        child: FutureBuilder<String>(
+          future: _getChannelId(), // a previously-obtained Future<String> or null
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              String message = errMsg != '' ? errMsg : 'Unable to get channel ID';
+              if (snapshot.data != '') {
+                message = 'Channel ID: ${snapshot.data}';
+              }
+
+              children = <Widget>[
+                Icon(
+                  Icons.check_circle_outline,
+                  color: snapshot.data != '' ? Colors.green : Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(message),
+                ),
+              ];
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting Channel ID...'),
+                ),
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
